d6c836f688e54f4f74f7f50bbb57915c9d7a986d
Ion Ostafi <ion.ostafi@ellation.com>
2019-09-01 15:27:33 +0300

    Fix threads1 and arc1

--- exercises/standard_library_types/arc1.rs
+++ exercises/standard_library_types/arc1.rs
@@ -4,15 +4,17 @@
 // somewhere. Try not to create any copies of the `numbers` Vec!
 // Scroll down for hints :)
 
-use std::sync::Arc;
+use std::sync::{Arc, Mutex};
 use std::thread;
+
 
 fn main() {
     let numbers: Vec<_> = (0..100u32).collect();
-    let shared_numbers = // TODO
+    let shared_numbers = Arc::new(numbers);
     let mut joinhandles = Vec::new();
 
     for offset in 0..8 {
+        let child_numbers = shared_numbers.clone();
         joinhandles.push(thread::spawn(move || {
             let mut i = offset;
             let mut sum = 0;
@@ -23,7 +25,7 @@ fn main() {
             println!("Sum of offset {} is {}", offset, sum);
         }));
     }
-    for handle in joinhandles.into_iter() {
+    for handle in joinhandles {
         handle.join().unwrap();
     }
 }
